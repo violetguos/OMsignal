609d44744c745f9fbca22827ecc8316f6b6d3d3c
Y. Violet Guo <yueguo598@gmail.com>
2019-03-15 17:52:26 -0400

    Test the final eval code andshell script

--- evaluation/run_evaluation.sh
+++ evaluation/run_evaluation.sh
@@ -6,8 +6,7 @@ source /rap/jvb-000-aa/COURS2019/etudiants/common.env
 # PROJECT_PATH will be changed to the master branch of your repo
 PROJECT_PATH='/rap/jvb-000-aa/COURS2019/etudiants/submissions/b2pomt1/code'
 
-
-RESULTS_DIR='/rap/jvb-000-aa/COURS2019/etudiants/projects/omsignal/evaluation'
-DATA_FILE='/rap/jvb-000-aa/COURS2019/etudiants/data/omsignal/myHeartProject/sample_test.dat'
+RESULTS_DIR='/rap/jvb-000-aa/COURS2019/etudiants/submissions/b2pomt1/code/evaluation/'
+DATA_FILE='/rap/jvb-000-aa/COURS2019/etudiants/data/omsignal/myHeartProject/MILA_ValidationLabeledData.dat'
 
 s_exec python $PROJECT_PATH/evaluation/eval.py --dataset=$DATA_FILE --results_dir=$RESULTS_DIR
