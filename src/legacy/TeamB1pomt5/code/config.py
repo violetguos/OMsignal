cbf05f24231dd45f99b4df7e6f25c2f8b59575d2
Tim <timothy.nest@gmail.com>
2019-03-15 12:06:29 -0400

    new branch for merge

--- src/legacy/TeamB1pomt5/code/config.py
+++ src/legacy/TeamB1pomt5/code/config.py
@@ -1,8 +1,9 @@
 import os
 
 ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
-DATA_DIR = "D:\JP\Documents\Maitrise\Hiver_2019\IFT6759_Machine_Learning_Project\Bloc_2_OM\OMsignal\data"
+DATA_DIR = os.path.join(ROOT_DIR, 'data')
 LOG_DIR = os.path.join(ROOT_DIR, 'log')
 MODELS_DIR = os.path.join(ROOT_DIR, 'models')
 FIGURES_DIR = os.path.join(ROOT_DIR, 'figures')
-CLUSTER_DIR = "/rap/jvb-000-aa/COURS2019/etudiants/data/omsignal/myHeartProject/"
+# CLUSTER_DIR = "/rap/jvb-000-aa/COURS2019/etudiants/data/omsignal/myHeartProject/"
+CLUSTER_DIR = "~/Dev/OMsignal_data/myHeartProject/"
