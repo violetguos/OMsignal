2ff242784c50f6de67b82e5acff8ea97234ee0a6
Y. Violet Guo <yueguo598@gmail.com>
2019-03-15 14:59:17 -0400

    PEP8 style refactor for algorithm class definitions

--- src/algorithm/torchdiffeq/_impl/fixed_grid.py
+++ src/algorithm/torchdiffeq/_impl/fixed_grid.py
@@ -1,4 +1,5 @@
-"""Code referenced from open source implementation at https://github.com/rtqichen/torchdiffeq/, code is taken as is without re-writing according to IFT6759 standards."""
+"""Code referenced from open source implementation at https://github.com/rtqichen/torchdiffeq/,
+code is taken as is without re-writing according to IFT6759 standards."""
 from .solvers import FixedGridODESolver
 from . import rk_common
 
